7e6cd9e4deaca9db8b491a38e9471dde9d1dec55
Tooma81 <toomas.kaarel@gmail.com>
2026-01-27 11:58:03 +0200

    App.js fix

--- client/src/App.js
+++ client/src/App.js
@@ -31,75 +31,13 @@ function smoothScrollToElement(id) {
   requestAnimationFrame(step);
 }
 
-function HomePage() {
-  const navigate = useNavigate();
 
-  return (
-    <div className="app">
-      <header className="app-header">
-        <h1>Külvikalender</h1>
-      </header>
-
-      <div className="content-grid">
-        <div 
-          className="content-section card-general"
-          style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/Img/Test5.png)` }}
-        >
-          <div className="card-general-content">
-            <h2>Üldine</h2>
-            <p className="card-subtitle">Kogu informatsioon ühes kohas!</p>
-            <button className="card-button" onClick={() => smoothScrollToElement('calendar')}>
-              Vaata kogu külvikalendrit
-            </button>
-          </div>
-        </div>
-
-        <div 
-          className="content-section card-personal"
-          style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/Img/Test6.png)` }}
-        >
-          <div className="card-personal-content">
-            <h2>Personaalne</h2>
-            <p className="card-subtitle">Loo endale meelepärane kalender ning prindi see!</p>
-            <button className="card-button" onClick={() => navigate('/kalender')}>
-              Minu külvikalender
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div className="info-section">
-        <h2>Mis on külvikalender?</h2>
-        <div className="info-text-container">
-          <p>
-            <b>Külvikalender on juhend või ajakava</b>, mis aitab planeerida aiatöid
-            kogu kasvuperioodi jooksul. See näitab, millal on kõige sobivam aeg erinevaid
-            taimi külvata, ette kasvatada, istutada, ümber istutada ja saaki koristada. 
-            Külvikalender arvestab taimede kasvuvajadusi ning kohalikke ilmastiku- ja kliimatingimusi, 
-            et taimed saaksid kasvada võimalikult soodsates oludes.
-          </p>
-          
-        </div>
-      </div>
-
-      <div className="content-section product-list-wrapper">
-        <ProductList />
-      </div>
-       
-      <div id="calendar" className="content-section">
-        <ActivityCalendar />
->>>>>>> a8cc70eb96da73708d8f0bc26c37b7fa3dae955b
-      </div>
-    </div>
-  );
-};
 
 function App() {
   return (
     <Router>
       <div className="app-wrapper">
         <Header />
-        <Breadcrumbs />
         <Routes>
           {/* Kasutame LandingPage komponenti, et mitte hoida kogu sisu App.js-is */}
           <Route path="/" element={<LandingPage />} />
